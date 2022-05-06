7b0c93559e1df178158fa68b61639cf756b8c0d4
irasemarivera <irasema.rivera@hotmail.com>
2022-05-06 18:13:52 +0200

    infura for goerli, dotenv for secrets and truffle verify plugin for etherscan

--- truffle-config.js
+++ truffle-config.js
@@ -1,24 +1,42 @@
+require('dotenv').config();
+
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.MNEMONIC_GOERLI;
+const infuraAccessKey = process.env.INFURA_ACCESS_KEY;
+
 module.exports = {
-// See <http://truffleframework.com/docs/advanced/configuration>
-// to customize your Truffle configuration!
-   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-networks: {
-   development: {
-      network_id: "*",
-      host: 'localhost',
-      port: 8545,
-      gas: 6721975,
-      gasPrice: 20000000000
+   // See <http://truffleframework.com/docs/advanced/configuration>
+   // to customize your Truffle configuration!
+      contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+   networks: {
+      development: {
+         network_id: "*",
+         host: 'localhost',
+         port: 8545,
+         gas: 6721975,
+         gasPrice: 20000000000
+      },
+      goerli: {
+         provider: function() {
+         return new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/" + infuraAccessKey)
+         },
+         network_id: 5
+      }
+   },
+   compilers: {
+      solc: {
+         version: "0.4.17",
+         optimizer: {
+            enabled: true,
+            runs: 200
+         }
+      }
+   },
+   plugins: [
+      'truffle-plugin-verify'
+    ],
+   api_keys: {
+      etherscan: process.env.ETHERSCAN_API_KEY
    }
-},
-compilers: {
-   solc: {
-       version: "0.4.17",
-       optimizer: {
-          enabled: true,
-          runs: 200
-       }
-   }
-}
 };
